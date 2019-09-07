906564e05032d40e3ec447243664f64a7194f76d
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-07 12:20:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   belongs_to :song
   
   def genre_name=(name)
-    self.genre = Genre.find_by_or_create_by(name: name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
   
   def genre_name
@@ -14,7 +14,7 @@ class Song < ActiveRecord::Base
   end
   
   def artist_name=(name)
-    self.artist = Artist.find_by_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
   
   def artist_name
