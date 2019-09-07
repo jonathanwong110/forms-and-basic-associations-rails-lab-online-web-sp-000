de744fe031375d6182943e29a8c570176a35f1de
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-07 12:09:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,14 @@ class Song < ActiveRecord::Base
   # add associations here
   belongs_to :artist
   belongs_to :song
+  
+  def genre_name
+  end
+  
+  def artist_name
+  end
+  
+  def note_contents
+  end
+  
 end
